0a9e1afe3b8b2e6be5c3c2983b38b7e9fe6d8c24
Fabian Sauter <sauter.fabian@mailbox.org>
2022-07-20 09:38:44 +0200

    Updated example to 1.9.0

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@
 
 int main(int argc, char** argv) {
     cpr::Response r = cpr::Get(cpr::Url{"https://api.github.com/repos/whoshuu/cpr/contributors"},
-                               cpr::Authentication{"user", "pass"},
+                               cpr::Authentication{"user", "pass", cpr::AuthMode::BASIC},
                                cpr::Parameters{{"anon", "true"}, {"key", "value"}});
     std::cout << "Status code: " << r.status_code << '\n';
     std::cout << "Header:\n";
-    for (const std::pair<std::string, std::string>& kv : r.header) {
+    for (const std::pair<const std::basic_string<char>, std::basic_string<char>>& kv : r.header) {
         std::cout << '\t' << kv.first << ':' << kv.second << '\n';
     }
     std::cout << "Text: " << r.text << '\n';
